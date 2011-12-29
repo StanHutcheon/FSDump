2eac8dc54de827cedc2e47991e26530358d277d3
Stanley Hutcheon <stanley.h@hotmail.co.uk>
2011-12-29 02:54:19 +0000

    0.0.2-1

---
+++ FSDump/cmds.vb
@@ -0,0 +1,204 @@
+﻿Module cmds
+    'this file only exists to make the other one look nicer.
+
+    Declare Sub Sleep Lib "kernel32" Alias "Sleep" (ByVal dwMilliseconds As Long)
+
+    Public HashChecked As Boolean = False
+    Public ipsw As String
+    Public outputdir As String = My.Computer.FileSystem.SpecialDirectories.Desktop + "\Telluride9A406.N94OS"
+    Public ipswhash As String = "869caa17e6b3176efb11b5de653ec8330d43b176"
+    Public tmp As String = My.Computer.FileSystem.SpecialDirectories.Temp
+    Public vfdecryptkey As String = "a31ffd506c6711c5a0c52c9f0a2f7208a2f63ad9dd40506e70d80ea20a981eb1312bc774"
+
+    'free bit of code for ya (the normal ShellWait doesn't work in command line VB apps, so i wrote my own. I never used it in this app though.
+    Public Sub ShellWait(ByVal file As String, ByVal args As String)
+        Dim proc As Process = New Process
+        proc.StartInfo.Arguments = " " + args
+        proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        proc.StartInfo.FileName = file
+        proc.Start()
+        Do Until proc.HasExited
+            For i = 0 To 5000000
+            Next
+        Loop
+        proc.WaitForExit()
+    End Sub
+
+    'Also another bit of code i wrote, a Delay code, Delay didn't work (again) in command line VB apps so i wrote my own.
+    'make sure to have:
+    ' Declare Sub Sleep Lib "kernel32" Alias "Sleep" (ByVal dwMilliseconds As Long)
+    '(without the ' ) in your module somwhere, but not in a Sub or Function.
+    'same for this bit:
+    Public Sub Delay(ByVal seconds As Integer)
+        Dim delaytime As Integer = seconds * 1000
+        Sleep(delaytime)
+    End Sub
+    'then just use the code e.g Delay(3) = a delay for 3 seconds
+    'neat huh?
+
+    Public Function SHA1Hash(ByVal file As String) As String
+        ' open file (as read-only)
+        Using reader As New System.IO.FileStream(file, IO.FileMode.Open, IO.FileAccess.Read)
+            Using sha1 As New System.Security.Cryptography.SHA1CryptoServiceProvider
+                ' hash contents of this stream
+                Dim hash() As Byte = sha1.ComputeHash(reader)
+                ' return formatted hash
+                Return ByteArrayToString(hash)
+            End Using
+        End Using
+    End Function
+
+    ' utility function to convert a byte array into a hex string
+    Private Function ByteArrayToString(ByVal arrInput() As Byte) As String
+        Dim sb As New System.Text.StringBuilder(arrInput.Length * 2)
+        For i As Integer = 0 To arrInput.Length - 1
+            sb.Append(arrInput(i).ToString("X2"))
+        Next
+        Return sb.ToString().ToLower
+    End Function
+
+    Public Sub Log(Optional ByVal item As String = "", Optional ByVal type As String = "Info", Optional ByVal LogNewLine As Boolean = False)
+        Dim final As String
+
+        If LogNewLine = True Then
+            type = ""
+            final = vbNewLine
+        Else
+            final = vbNewLine + "[" + type + "] " + item
+        End If
+        MakeTextFile(final, tmp + "\FSDumpLog.txt")
+    End Sub
+
+    Public Sub MakeTextFile(ByVal text As String, ByVal file As String)
+        My.Computer.FileSystem.WriteAllText(file, text, True)
+    End Sub
+
+    Public Sub DownloadFile(ByVal ipsw As String, ByVal dir As String)
+        Dim FileName = url_filename(ipsw)
+        Console.WriteLine(" ")
+        Console.WriteLine("[Info] Downloading " + FileName + "...")
+        Console.WriteLine(" ")
+        wget(ipsw, dir)
+        ipsw = dir + "\" + FileName
+    End Sub
+
+    Public Sub dmg(ByVal key As String, ByVal file As String, ByVal outfile As String)
+        ChDir(My.Application.Info.DirectoryPath + "\tmp")
+        Dim decrypt As Process = New Process
+        decrypt.StartInfo.Arguments = " extract " + file + " " + outfile + " -k " + key
+        decrypt.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        decrypt.StartInfo.FileName = My.Application.Info.DirectoryPath + "\tmp\dmg.exe"
+        decrypt.Start()
+        decrypt.WaitForExit()
+        'Log(LogNewLine:=True)
+        'Log("dmg log")
+        'Log(decrypt.StandardOutput.ReadToEnd)
+        'Log(LogNewLine:=True)
+    End Sub
+
+    Public Sub wget(ByVal file As String, ByVal dir As String)
+        Dim FileName = url_filename(file)
+        ChDir(My.Application.Info.DirectoryPath + "\tmp")
+        Dim wgetProc As Process = New Process
+        wgetProc.StartInfo.UseShellExecute = False
+        wgetProc.StartInfo.Arguments = " -O " + dir + "\" + FileName + " " + file
+        wgetProc.StartInfo.RedirectStandardOutput = True
+        wgetProc.StartInfo.FileName = My.Application.Info.DirectoryPath + "\tmp\wget.exe"
+        wgetProc.Start()
+        wgetProc.WaitForExit()
+        Log(LogNewLine:=True)
+        Log("wget log")
+        Log(wgetProc.StandardOutput.ReadToEnd)
+        Log(LogNewLine:=True)
+    End Sub
+
+    Private Function url_filename(ByVal file As String)
+        Dim intPos As Int32
+        intPos = file.LastIndexOfAny("/")
+        intPos += 1
+        Dim FileName As String = file.Substring(intPos, (Len(file) - intPos))
+        Return FileName
+    End Function
+
+    Public Sub unzip(ByVal file As String, Optional ByVal dir As String = "", Optional ByVal overwrite As Boolean = False)
+        ChDir(My.Application.Info.DirectoryPath + "\tmp")
+        Dim args As String = " x "
+        If Not dir = "" Then
+            args = args + "-o" + dir + " "
+        ElseIf overwrite = True Then
+            args = args + "-y "
+        End If
+        args = args + file
+        Dim sevenzip As Process = New Process
+        sevenzip.StartInfo.Arguments = args
+        'sevenzip.StartInfo.RedirectStandardOutput = True
+        'sevenzip.StartInfo.UseShellExecute = False
+        sevenzip.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        sevenzip.StartInfo.FileName = My.Application.Info.DirectoryPath + "\tmp\7z.exe"
+        sevenzip.Start()
+        Do Until sevenzip.HasExited
+            For i = 0 To 5000000
+            Next
+        Loop
+        sevenzip.WaitForExit()
+        'Log(LogNewLine:=True)
+        'Log("7z Log")
+        'Log(sevenzip.StandardOutput.ReadToEnd)
+        'Log(LogNewLine:=True)
+    End Sub
+
+    'ignore this shit, its for later.
+    Public Function partial_zip_callback(ByVal exe As String)
+        Dim i As Integer = 1
+        Dim partial_zip As Process = New Process
+        partial_zip.StartInfo.UseShellExecute = False
+        partial_zip.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        partial_zip.StartInfo.RedirectStandardOutput = True
+        partial_zip.StartInfo.FileName = " " + exe
+        partial_zip.Start()
+        'Console.Write("[                                                  ]")
+        Do Until i = 100
+            Console.Clear()
+            Dim Prog As String = "[                                                  ]"
+            Dim stdout As String = partial_zip.StandardOutput.ReadLine
+            Console.Write(Prog)
+            Console.CursorLeft = 1
+            If stdout.Contains(i.ToString + "%") Then
+                If i = 1 Then
+                    Console.CursorLeft = 2
+                    Console.Write("=>")
+                ElseIf Not i = 1 Then
+                    Console.CursorLeft = 1
+                    Console.Write("=")
+                End If
+                Prog = Console.Read.ToString
+                Prog.Remove(50 - i, i)
+                i = i + 1
+                stdout = ""
+            End If
+        Loop
+        partial_zip.WaitForExit()
+    End Function
+
+    Public Sub Cleanup()
+        If System.IO.Directory.Exists(tmp + "\fs") Then
+            System.IO.Directory.Delete(tmp + "\fs", True)
+        End If
+        If System.IO.Directory.Exists(tmp + "\xtractedfs\5\Telluride9A406.N94OS\System") Then
+            System.IO.Directory.Delete(tmp + "\xtractedfs\5\Telluride9A406.N94OS\System", True)
+        End If
+        If System.IO.Directory.Exists(tmp + "\xtractedfs\5\Telluride9A406.N94OS") Then
+            System.IO.Directory.Delete(tmp + "\xtractedfs\5\Telluride9A406.N94OS", True)
+        End If
+        If System.IO.Directory.Exists(tmp + "\xtractedfs\5") Then
+            System.IO.Directory.Delete(tmp + "\xtractedfs\5", True)
+        End If
+        If System.IO.Directory.Exists(tmp + "\xtractedfs") Then
+            System.IO.Directory.Delete(tmp + "\xtractedfs", True)
+        End If
+        If System.IO.File.Exists(tmp + "\decrypted.038-3763-001.dmg.iso") Then
+            System.IO.File.Delete(tmp + "\decrypted.038-3763-001.dmg.iso")
+        End If
+    End Sub
+
+End Module
--- FSDump/main.vb
+++ FSDump/main.vb
@@ -1,40 +1,22 @@
 ﻿Module main
-    Declare Sub Sleep Lib "kernel32" Alias "Sleep" (ByVal dwMilliseconds As Long)
-
-    Public ipsw As String
-    Public tmp As String = My.Computer.FileSystem.SpecialDirectories.Temp
-    Public vfdecryptexe As Process = New Process
-    Public vfdecryptkey As String = "a31ffd506c6711c5a0c52c9f0a2f7208a2f63ad9dd40506e70d80ea20a981eb1312bc774"
-    Public HasRead As Boolean = False
-
-    'free bit of code for ya (the normal ShellWait doesn't work in command line VB apps, so i wrote my own. I never used it in this app though.
-    Public Sub ShellWait(ByVal file As String, ByVal args As String)
-        Dim proc As Process = New Process
-        proc.StartInfo.Arguments = " " + args
-        proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
-        proc.StartInfo.FileName = file
-        proc.Start()
-        Do Until proc.HasExited
-            For i = 0 To 5000000
-            Next
-        Loop
-        proc.WaitForExit()
-    End Sub
-
-    'Also another bit of code i wrote, a Delay code, Delay didn't work (again) in command line VB apps so i wrote my own.
-    'make sure to have:
-    ' Declare Sub Sleep Lib "kernel32" Alias "Sleep" (ByVal dwMilliseconds As Long)
-    '(without the ' ) in your module somwhere, but not in a Sub or Function.
-    'same for this bit:
-    Public Sub Delay(ByVal seconds As Integer)
-        Dim delaytime As Integer = seconds * 1000
-        Sleep(delaytime)
-    End Sub
-    'then just use the code e.g Delay(3) = a delay for 3 seconds
-    'neat huh?
 
     Sub Main()
-        Console.Title = "FSDump - iPhone4,1 iOS 5.0.1 9A406"
+        Log(TimeOfDay.Date + " " + TimeOfDay.TimeOfDay.ToString)
+        Log("--System information--")
+        Log(My.Computer.Info.AvailablePhysicalMemory, "System")
+        Log(My.Computer.Info.AvailableVirtualMemory, "System")
+        Log(My.Computer.Info.InstalledUICulture.ToString, "System")
+        Log(My.Computer.Info.OSFullName, "System")
+        Log(My.Computer.Info.OSPlatform, "System")
+        Log(My.Computer.Info.OSVersion, "System")
+        Log(My.Computer.Info.TotalPhysicalMemory, "System")
+        Log(My.Computer.Info.TotalVirtualMemory, "System")
+        Log(LogNewLine:=True)
+        Log("FSDump 0.0.2-1")
+        Log(LogNewLine:=True)
+
+        Console.Write(" ")
+        Console.Title = "FSDump 0.0.2-1 - iPhone4,1 iOS 5.0.1 9A406"
         Dim a_strArgs() As String
 
         Dim i As Integer
@@ -43,7 +25,7 @@
         For i = LBound(a_strArgs) To UBound(a_strArgs)
             Select Case LCase(a_strArgs(i))
                 Case "-h"
-                    'Console.WriteLine("usage: -i <ipsw>")
+                    Console.WriteLine("usage: FSDump.exe [-i <ipsw>]")
                     Exit Sub
                 Case "-i"
                     If i = UBound(a_strArgs) Then
@@ -58,179 +40,175 @@
                         ipsw = a_strArgs(i)
                     End If
                 Case Else
-                    'Console.WriteLine("usage: -i <ipsw>")
-                    Console.WriteLine("Default to download IPSW...")
+                    If ipsw = "" Then
+                        Console.WriteLine("  *Set default = Download IPSW")
+                        Log("Used downloaded ipsw")
+                        Log(LogNewLine:=True)
+
+                    End If
                     Console.WriteLine(" ")
-                    'Exit Sub
             End Select
         Next
-        Console.WriteLine("--FSDump for iPhone 4S - iOS 5.1--")
+
+        Console.WriteLine("--FSDump 0.0.2-1 for iPhone 4S - iOS 5.0.1--")
         Console.WriteLine(" ")
         Console.WriteLine("This app wouldn't have been made possible if it wasn't for these developers:")
         Console.WriteLine(" ")
         Console.WriteLine(" Igor Pavlov - 7-Zip")
-        Console.WriteLine(" planetbeing - vfdecrypt (xpwn)")
+        Console.WriteLine(" planetbeing - dmg (xpwn)")
         Console.WriteLine(" Apple - iOS")
         Console.WriteLine(" Microsoft - Visual Studio")
         Console.WriteLine(" ")
-        Console.WriteLine("Cleaning up old files...")
+
+        'clean previous stuff
+        Console.WriteLine("[Info] Cleaning up old files...")
+        Console.WriteLine(" ")
         Try
             Cleanup()
         Catch ex As Exception
-            Console.WriteLine("Error code 1 'Failed to remove previously used directories'")
+            Console.WriteLine("[Info - Error] Error code 1 'Failed to remove previously used directories (is 7z.exe still running?)'")
+            Log(ex.ToString, "Error")
+
             Exit Sub
         End Try
-        Console.WriteLine(" ")
-        Console.WriteLine("Downloading IPSW...")
-        wget("http://appldnld.apple.com/iPhone4//041-3417.20111215.Slnt4/iPhone4,1_5.0.1_9A406_Restore.ipsw", tmp)
-        ipsw = tmp + "\iPhone4,1_5.0.1_9A406_Restore.ipsw"
-        Console.WriteLine(" ")
-        'extract
-        Console.WriteLine("Unzipping ipsw...")
-        Console.WriteLine(" ")
-        Try
-            'Using zip1 As ZipFile = ZipFile.Read(ipsw)
-            'zip1.Extract("038-3074-006.dmg", tmp)
-            'End Using
-            unzip(ipsw, tmp + "\fs", True)
-        Catch ex As Exception
-            Console.WriteLine("Error code 2 'Failed to extract IPSW (corrupt file?)'")
-            Exit Sub
-        End Try
-        If Not System.IO.File.Exists(tmp + "\fs\038-3763-001.dmg") Then
-            Console.WriteLine("This isn't an iPhone 4S ipsw.")
-            Exit Sub
+
+        'xtra download stuff
+        If ipsw = "" Then
+            Try
+                'Console.WriteLine("[Info] Downloading 038-3763-001.dmg from iPhone4,1_5.0.1_9A406_Restore.ipsw...")
+                If System.IO.File.Exists(tmp + "\iPhone4,1_5.0.1_9A406_Restore.ipsw") Then
+                    Console.WriteLine("[Info] '" + tmp + "\iPhone4,1_5.0.1_9A406_Restore.ipsw' already exists.")
+                    Console.WriteLine("[Info] Checking IPSW...")
+                    Log("IPSW already exists, checking SHA1 hash to see if its legit...")
+                    Log(LogNewLine:=True)
+                    If SHA1Hash(tmp + "\iPhone4,1_5.0.1_9A406_Restore.ipsw") = ipswhash Then
+                        Console.WriteLine("[Info] IPSW SHA1 hash matched: " + ipswhash + "")
+                        Log("IPSW is legit, skipping next SHA1 check...")
+                        Log(LogNewLine:=True)
+                        HashChecked = True
+                    Else
+                        Log("IPSW isn't legit. Nice try.")
+                        Log(LogNewLine:=True)
+                        DownloadFile("http://appldnld.apple.com/iPhone4//041-3417.20111215.Slnt4/iPhone4,1_5.0.1_9A406_Restore.ipsw", tmp)
+                        Log("Downloaded IPSW")
+                        Log(LogNewLine:=True)
+                    End If
+                End If
+                'partial_zip("http://appldnld.apple.com/iPhone4//041-3417.20111215.Slnt4/iPhone4,1_5.0.1_9A406_Restore.ipsw", "038-3763-001.dmg", tmp + "\fs\038-3763-001.dmg")
+            Catch ex As Exception
+                Console.WriteLine("[Info - Error] Error code 2 'Failed to download IPSW'")
+                Log("Can't download IPSW, probably internet", "Error")
+
+                Exit Sub
+            End Try
+            Console.WriteLine(" ")
+            ipsw = tmp + "\iPhone4,1_5.0.1_9A406_Restore.ipsw"
+            Log(ipsw)
+            Log(LogNewLine:=True)
+
         End If
 
-        'decrypt
-        Console.WriteLine("Decrypting RootFS [038-3763-001.dmg]...")
+        'check ipsw hash
+        If HashChecked = False Then
+            Console.WriteLine("[Info] Checking IPSW...")
+            Console.WriteLine(" ")
+            If Not SHA1Hash(ipsw) = ipswhash Then
+                Console.WriteLine("This is not a valid iPhone 4S 5.0.1 9A406 IPSW")
+                Console.WriteLine("Make sure this is the 9A406 IPSW and not the 9A405 one as that does not have decrypted ramdisks (so no vfdecrypt key)")
+                Log("Y U NO PICK IPHONE 4S IPSW??????????", "Error")
+                Log(LogNewLine:=True)
+
+                Exit Sub
+            Else
+                Console.WriteLine("[Info] IPSW SHA1 hash matched: " + ipswhash + "")
+                Log("SHA1 matched! " + ipswhash)
+                Log(LogNewLine:=True)
+                Console.WriteLine(" ")
+            End If
+        End If
+
+        Console.WriteLine("[Info] Unzipping IPSW...")
         Console.WriteLine(" ")
         Try
-            vfdecrypt(vfdecryptkey, tmp + "\fs\0038-3763-001.dmg", tmp + "\decrypted.038-3763-001.dmg")
+            unzip(ipsw, tmp + "\fs", True)
         Catch ex As Exception
-            Console.WriteLine("Error code 3 'Failed to decrypt firmware image'")
+            Console.WriteLine("Error code 3 'Failed to extract IPSW'")
+            Log("Failed to unzip the ipsw provided, it cant be corrupt because it passed SHA1 checks, Probably unrelated 7z issue" + vbNewLine + ex.ToString, "Error")
+            Log(LogNewLine:=True)
+
+            Cleanup()
             Exit Sub
         End Try
-        Delay(2)
 
-        'extract again
-        Console.WriteLine("Extracting RootFS [decrypted.038-3763-001.dmg]...")
+        Console.WriteLine("[Info] Decrypting RootFS [038-3763-001.dmg]...")
         Console.WriteLine(" ")
         Try
-            unzip(tmp + "\decrypted.038-3763-001.dmg", tmp + "\xtractedfs", True)
+            dmg(vfdecryptkey, tmp + "\fs\038-3763-001.dmg", tmp + "\decrypted.038-3763-001.dmg.iso")
         Catch ex As Exception
-            Console.WriteLine("Error code 4 'Failed to extract decrypted firmware image'")
+            Console.WriteLine("Error code 4 'Failed to decrypt firmware image'")
+            Log("pile of horse shit" + vbNewLine + ex.ToString, "Error")
+            Log(LogNewLine:=True)
+
+            Cleanup()
             Exit Sub
         End Try
-        Delay(2)
 
-        'extract AGAIN
-        Console.WriteLine("Extracting hfsx Filesystem [5.hfsx]... **this may take a while**")
-        Console.WriteLine(" ")
-        Try
-            unzip(tmp + "\xtractedfs\5.hfsx", tmp + "\xtractedfs\5", True)
-        Catch ex As Exception
-            Console.WriteLine("Error code 5 'Failed to extract hfsx filesystem'")
-            Exit Sub
-        End Try
-        Delay(2)
 
-        'copy fs
-        Console.WriteLine("Dumping filesystem [Telluride9A406.N94OS]...")
+        'extract
+        Console.WriteLine("[Info] Checking directories...")
         Console.WriteLine(" ")
-        If System.IO.Directory.Exists(My.Computer.FileSystem.SpecialDirectories.Desktop + "\4S dump") Then
-            Console.WriteLine("The directory '" + My.Computer.FileSystem.SpecialDirectories.Desktop + "\4S dump' already exists. Would you like to overwrite? [y|n]:")
+        If System.IO.Directory.Exists(outputdir) Then
+            'overwrite?
+            Console.WriteLine("The directory '" + outputdir + "' already exists." + vbNewLine + "Would you like to overwrite? [y|n]:")
+            Dim HasRead As Boolean = False
             Do
                 Dim ans = Console.ReadLine
                 If ans = "y" Or ans = "yes" Then
                     HasRead = True
-                    System.IO.Directory.Delete(My.Computer.FileSystem.SpecialDirectories.Desktop + "\4S dump", True)
+                    Console.WriteLine("[Info] Deleting " + outputdir + "...")
+                    Console.WriteLine(" ")
+                    Try
+                        My.Computer.FileSystem.DeleteDirectory(outputdir, FileIO.DeleteDirectoryOption.DeleteAllContents)
+                        System.IO.Directory.Delete(outputdir, True)
+                    Catch ex As Exception
+                        Log(ex.ToString, "Error")
+                        Log(LogNewLine:=True)
+
+                    End Try
+                    Console.WriteLine("[Info] Dumping filesystem [Telluride9A406.N94OS] - Takes quite a long time...")
                 ElseIf ans = "n" Or ans = "no" Then
                     HasRead = True
                     Exit Sub
                 End If
             Loop Until HasRead = True
-            Console.WriteLine(" ")
         End If
         Try
-            System.IO.Directory.Move(tmp + "\xtractedfs\5\Telluride9A406.N94OS", My.Computer.FileSystem.SpecialDirectories.Desktop + "\4S dump")
-            Console.WriteLine("Dumped to: " + My.Computer.FileSystem.SpecialDirectories.Desktop + "\4S Dump")
+            MkDir(outputdir)
+            Delay(2)
+            'ChDir(tmp)
+            unzip(tmp + "\decrypted.038-3763-001.dmg.iso", outputdir)
+            Log("unzipped ipsw yey ^^")
+            Log(LogNewLine:=True)
+
+            Console.ReadLine()
+            Console.WriteLine("")
+            Console.WriteLine("Dumped to: " + My.Computer.FileSystem.SpecialDirectories.Desktop + "\Telluride9A406.N94OS")
             Console.WriteLine(" ")
             Console.WriteLine("*note: all plists within the dump don't contain text")
             Console.WriteLine(" ")
-            Console.WriteLine("Cleaning up files...")
+            Console.WriteLine("[Info] Cleaning up files...")
             Cleanup()
         Catch ex As Exception
-            Console.WriteLine("Error code 6 'Failed to move directory '" + tmp + "\xtractedfs\5\HoodooVail9B5117b.N94DeveloperOS' to desktop (get them yourself)")
+            Console.WriteLine("[Info - Error] Error code 5 'Failed to extract dmg '" + tmp + "\decrypted.038-3763-001.dmg.iso' to desktop (get it yourself?)")
+            Log(ex.ToString, "Error")
+            Log(LogNewLine:=True)
+
             Exit Sub
         End Try
-    End Sub
 
-    Public Sub vfdecrypt(ByVal key As String, ByVal file As String, ByVal outfile As String)
-        ChDir(My.Application.Info.DirectoryPath + "\tmp")
-        Dim decrypt As Process = New Process
-        decrypt.StartInfo.Arguments = " -i " + file + " -k " + key + " -o " + outfile
-        decrypt.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
-        decrypt.StartInfo.FileName = My.Application.Info.DirectoryPath + "\tmp\vfdecrypt.exe"
-        decrypt.Start()
-        decrypt.WaitForExit()
-    End Sub
+        Log("***END LOG***")
+        Log(LogNewLine:=True)
 
-    Public Sub wget(ByVal file As String, ByVal dir As String)
-        Dim intPos As Int32
-        intPos = file.LastIndexOfAny("/")
-        intPos += 1
-        Dim FileName As String = file.Substring(intPos, (Len(file) - intPos))
-        ChDir(My.Application.Info.DirectoryPath + "\tmp")
-        Dim wget As Process = New Process
-        wget.StartInfo.UseShellExecute = False
-        wget.StartInfo.Arguments = " -O " + dir + "\" + FileName + " " + file
-        wget.StartInfo.RedirectStandardOutput = True
-        wget.StartInfo.FileName = My.Application.Info.DirectoryPath + "\tmp\wget.exe"
-        wget.Start()
-        wget.WaitForExit()
-    End Sub
-
-    Public Sub unzip(ByVal file As String, Optional ByVal dir As String = "", Optional ByVal overwrite As Boolean = False)
-        ChDir(My.Application.Info.DirectoryPath + "\tmp")
-        Dim args As String = " x "
-        If Not dir = "" Then
-            args = args + "-o" + dir + " "
-        ElseIf overwrite = True Then
-            args = args + "-y "
-        End If
-        args = args + file
-        Dim sevenzip As Process = New Process
-        sevenzip.StartInfo.Arguments = args
-        sevenzip.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
-        sevenzip.StartInfo.FileName = My.Application.Info.DirectoryPath + "\tmp\7z.exe"
-        sevenzip.Start()
-        Do Until sevenzip.HasExited
-            For i = 0 To 5000000
-            Next
-        Loop
-        sevenzip.WaitForExit()
-    End Sub
-
-    Public Sub Cleanup()
-        If System.IO.Directory.Exists(tmp + "\fs") Then
-            System.IO.Directory.Delete(tmp + "\fs", True)
-        End If
-        If System.IO.Directory.Exists(tmp + "\xtractedfs\5\HoodooVail9B5117b.N94DeveloperOS\System") Then
-            System.IO.Directory.Delete(tmp + "\xtractedfs\5\HoodooVail9B5117b.N94DeveloperOS\System", True)
-        End If
-        If System.IO.Directory.Exists(tmp + "\xtractedfs\5\HoodooVail9B5117b.N94DeveloperOS") Then
-            System.IO.Directory.Delete(tmp + "\xtractedfs\5\HoodooVail9B5117b.N94DeveloperOS", True)
-        End If
-        If System.IO.Directory.Exists(tmp + "\xtractedfs\5") Then
-            System.IO.Directory.Delete(tmp + "\xtractedfs\5", True)
-        End If
-        If System.IO.Directory.Exists(tmp + "\xtractedfs") Then
-            System.IO.Directory.Delete(tmp + "\xtractedfs", True)
-        End If
-        If System.IO.File.Exists(tmp + "\decrypted.038-3074-006.dmg") Then
-            System.IO.File.Delete(tmp + "\decrypted.038-3074-006.dmg")
-        End If
     End Sub
 
 End Module
